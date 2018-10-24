b7b8b18444c4baba7cc6f1f3fa8628d7c15be1aa
hamid <hd.karimpoor@gmail.com>
2018-10-23 23:39:12 -0400

    add about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
